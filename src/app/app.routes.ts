fb54812fea6bad9813249e6157c27d385f512db5
Ahmad Hakam <ahmaed.hakam@gmail.com>
2024-12-11 09:00:44 +0300

    routes fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,23 @@ import { MoonComponent } from './planet/moon/moon.component';
 import { MarsComponent } from './planet/mars/mars.component';
 
 export const routes: Routes = [
-  { path: 'moon', component: MoonComponent },
-  { path: 'mars', component: MarsComponent },
-  { path: '', redirectTo: 'moon', pathMatch: 'full' },
-  { path: '**', redirectTo: 'moon' }
+  {
+    path: 'moon',
+    loadComponent: () => import('./planet/moon/moon.component').then((m) => m.MoonComponent),
+  },
+  {
+    path: 'mars',
+    loadComponent: () => import('./planet/mars/mars.component').then((m) => m.MarsComponent),
+  },
+  {
+    path: '**',
+    redirectTo: "moon",
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    redirectTo: "moon",
+    pathMatch: 'full',
+  },
+
 ];
